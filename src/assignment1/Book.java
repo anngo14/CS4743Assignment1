60b821d8f10824787caf1067dd3fe0a5b2508986
anngo14 <31315148+anngo14@users.noreply.github.com>
2019-01-22 12:03:13 -0600

    book object

---
+++ src/assignment1/Book.java
@@ -0,0 +1,56 @@
+package assignment1;
+
+public class Book {
+
+	private String bookTitle;
+	private String bookISBN;
+	private String bookSummary;
+	private int bookYearPublished;
+	
+	public Book()
+	{
+		bookTitle = "";
+		bookISBN = "";
+		bookSummary = "";
+		bookYearPublished = 0;
+	}
+	public Book(String title, String isbn, String summary, int year)
+	{
+		bookTitle = title;
+		bookISBN = isbn;
+		bookSummary = summary;
+		bookYearPublished = year;
+	}
+	public String getTitle()
+	{
+		return bookTitle;
+	}
+	public String getISBN()
+	{
+		return bookISBN;
+	}
+	public String getSummary()
+	{
+		return bookSummary;
+	}
+	public int getYear()
+	{
+		return bookYearPublished;
+	}
+	public void setTitle(String title)
+	{
+		bookTitle = title;
+	}
+	public void setISBN(String isbn)
+	{
+		bookISBN = isbn;
+	}
+	public void setSummary(String summary)
+	{
+		bookSummary = summary;
+	}
+	public void setYear(int year)
+	{
+		bookYearPublished = year;
+	}
+}
