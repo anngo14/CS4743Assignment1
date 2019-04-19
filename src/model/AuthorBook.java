a95bae7c942357908f3696a010676f1073f15468
Richard Azille <richieazille@gmail.com>
2019-04-19 15:18:34 -0500

    AuthorBook Model Implemented
    - Associated author_book junction table created in database

---
+++ src/model/AuthorBook.java
@@ -0,0 +1,60 @@
+package model;
+
+public class AuthorBook {
+	private Author author;
+	private Book book;
+	private int royalty;
+	private boolean newRecord = true;
+	
+	public AuthorBook(Author author, Book book, int royalty) {
+		this.author = author;
+		this.book = book;
+		this.royalty = royalty * 100000;
+	}
+	
+	public AuthorBook(Author author, Book book, int royalty, boolean newRecord) {
+		this.author = author;
+		this.book = book;
+		this.royalty = royalty * 100000;
+		this.newRecord = newRecord;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getRoyalty() {
+		return royalty;
+	}
+
+	public void setRoyalty(int royalty) {
+		this.royalty = royalty;
+	}
+
+	public boolean isNewRecord() {
+		return newRecord;
+	}
+
+	public void setNewRecord(boolean newRecord) {
+		this.newRecord = newRecord;
+	}
+
+	@Override
+	public String toString() {
+		return "AuthorBook [author=" + author + ", book=" + book + ", royalty=" + royalty + ", newRecord=" + newRecord
+				+ "]";
+	}
+	
+}
